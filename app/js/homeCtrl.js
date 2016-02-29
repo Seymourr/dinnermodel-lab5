953d7383b60b4fb762d9a7d234d3f7f788974dbf
Seymourr <jack_shabo6@hotmail.com>
2016-02-29 22:31:30 +0100

    Started working on angularjs solution

---
+++ app/js/homeCtrl.js
@@ -0,0 +1,4 @@
+
+dinnerPlannerApp.controller('HomeCtrl', function ($scope,Dinner) {
+  $scope.bodyStyle = {background:"url('images/firstpage.jpg')"; backgroundSize: "100%"; position: "static";};
+});
